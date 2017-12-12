ce83d9f9ed6b9c5a2b55061e582fd8e125f9835a
lauro <laurogn@gmail.com>
2017-12-12 19:30:35 -0300

    upload projeto

---
+++ src/main/java/br/com/ciceroednilson/repository/PessoaRepository.java
@@ -0,0 +1,130 @@
+package br.com.ciceroednilson.repository;
+ 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+ 
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+ 
+import br.com.ciceroednilson.model.PessoaModel;
+import br.com.ciceroednilson.model.UsuarioModel;
+import br.com.ciceroednilson.repository.entity.PessoaEntity;
+import br.com.ciceroednilson.repository.entity.UsuarioEntity;
+import br.com.ciceroednilson.uteis.Uteis;
+ 
+public class PessoaRepository {
+ 
+	@Inject
+	PessoaEntity pessoaEntity;
+ 
+	EntityManager entityManager;
+ 
+	/***
+	 * MÉTODO RESPONSÁVEL POR SALVAR UMA NOVA PESSOA
+	 * @param pessoaModel
+	 */
+	public void SalvarNovoRegistro(PessoaModel pessoaModel){
+ 
+		entityManager =  Uteis.JpaEntityManager();
+ 
+		pessoaEntity = new PessoaEntity();
+		pessoaEntity.setDataCadastro(LocalDateTime.now());
+		pessoaEntity.setEmail(pessoaModel.getEmail());
+		pessoaEntity.setEndereco(pessoaModel.getEndereco());
+		pessoaEntity.setNome(pessoaModel.getNome());
+		pessoaEntity.setOrigemCadastro(pessoaModel.getOrigemCadastro());
+		pessoaEntity.setSexo(pessoaModel.getSexo());
+ 
+		UsuarioEntity usuarioEntity = entityManager.find(UsuarioEntity.class, pessoaModel.getUsuarioModel().getCodigo()); 
+ 
+		pessoaEntity.setUsuarioEntity(usuarioEntity);
+ 
+		entityManager.persist(pessoaEntity);
+ 
+	}
+	
+	/***
+	 * CONSULTA UMA PESSOA CADASTRADA PELO CÓDIGO
+	 * @param codigo
+	 * @return
+	 */
+	private PessoaEntity GetPessoa(int codigo){
+ 
+		entityManager =  Uteis.JpaEntityManager();
+ 
+		return entityManager.find(PessoaEntity.class, codigo);
+	}
+ 
+	/***
+	 * ALTERA UM REGISTRO CADASTRADO NO BANCO DE DADOS
+	 * @param pessoaModel
+	 */
+	public void AlterarRegistro(PessoaModel pessoaModel){
+ 
+		entityManager =  Uteis.JpaEntityManager();
+ 
+		PessoaEntity pessoaEntity = this.GetPessoa(pessoaModel.getCodigo());
+ 
+		pessoaEntity.setEmail(pessoaModel.getEmail());
+		pessoaEntity.setEndereco(pessoaModel.getEndereco());
+		pessoaEntity.setNome(pessoaModel.getNome());
+		pessoaEntity.setSexo(pessoaModel.getSexo());
+ 
+		entityManager.merge(pessoaEntity);
+	}
+
+ 
+	/***
+	 * MÉTODO PARA CONSULTAR A PESSOA
+	 * @return
+	 */
+	public List<PessoaModel> GetPessoas(){
+ 
+		List<PessoaModel> pessoasModel = new ArrayList<PessoaModel>();
+ 
+		entityManager =  Uteis.JpaEntityManager();
+ 
+		Query query = entityManager.createNamedQuery("PessoaEntity.findAll");
+ 
+		@SuppressWarnings("unchecked")
+		Collection<PessoaEntity> pessoasEntity = (Collection<PessoaEntity>)query.getResultList();
+ 
+		PessoaModel pessoaModel = null;
+ 
+		for (PessoaEntity pessoaEntity : pessoasEntity) {
+ 
+			pessoaModel = new PessoaModel();
+			pessoaModel.setCodigo(pessoaEntity.getCodigo());
+			pessoaModel.setDataCadastro(pessoaEntity.getDataCadastro());
+			pessoaModel.setEmail(pessoaEntity.getEmail());
+			pessoaModel.setEndereco(pessoaEntity.getEndereco());
+			pessoaModel.setNome(pessoaEntity.getNome());
+ 
+			if(pessoaEntity.getOrigemCadastro().equals("X"))
+				pessoaModel.setOrigemCadastro("XML");
+			else
+				pessoaModel.setOrigemCadastro("INPUT");
+ 
+ 
+			if(pessoaEntity.getSexo().equals("M"))
+				pessoaModel.setSexo("Masculino");
+			else
+				pessoaModel.setSexo("Feminino");
+ 
+			UsuarioEntity usuarioEntity =  pessoaEntity.getUsuarioEntity();			
+ 
+			UsuarioModel usuarioModel = new UsuarioModel();
+			usuarioModel.setUsuario(usuarioEntity.getUsuario());
+ 
+			pessoaModel.setUsuarioModel(usuarioModel);
+ 
+			pessoasModel.add(pessoaModel);
+		}
+ 
+		return pessoasModel;
+ 
+	}
+}
